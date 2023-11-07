fcbacc5ce63dfd7e0f0b42b739d03a457c47da78
sudharshan1234 <sudarchakra2000@gmail.com>
2023-11-07 18:03:59 -0500

    initial commit

---
+++ src/controller/ExpenseTrackerController.java
@@ -0,0 +1,75 @@
+package controller;
+
+import view.ExpenseTrackerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import model.ExpenseTrackerModel;
+import model.Transaction;
+import model.Filter.TransactionFilter;
+
+public class ExpenseTrackerController {
+  
+  private ExpenseTrackerModel model;
+  private ExpenseTrackerView view;
+  /** 
+   * The Controller is applying the Strategy design pattern.
+   * This is the has-a relationship with the Strategy class 
+   * being used in the applyFilter method.
+   */
+  private TransactionFilter filter;
+
+  public ExpenseTrackerController(ExpenseTrackerModel model, ExpenseTrackerView view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  public void setFilter(TransactionFilter filter) {
+    // Sets the Strategy class being used in the applyFilter method.
+    this.filter = filter;
+  }
+
+  public void refresh() {
+    List<Transaction> transactions = model.getTransactions();
+    view.refreshTable(transactions);
+  }
+
+  public boolean addTransaction(double amount, String category) {
+    if (!InputValidation.isValidAmount(amount)) {
+      return false;
+    }
+    if (!InputValidation.isValidCategory(category)) {
+      return false;
+    }
+    
+    Transaction t = new Transaction(amount, category);
+    model.addTransaction(t);
+    view.getTableModel().addRow(new Object[]{t.getAmount(), t.getCategory(), t.getTimestamp()});
+    refresh();
+    return true;
+  }
+
+  public void applyFilter() {
+    //null check for filter
+    if(filter!=null){
+      // Use the Strategy class to perform the desired filtering
+      List<Transaction> transactions = model.getTransactions();
+      List<Transaction> filteredTransactions = filter.filter(transactions);
+      List<Integer> rowIndexes = new ArrayList<>();
+      for (Transaction t : filteredTransactions) {
+        int rowIndex = transactions.indexOf(t);
+        if (rowIndex != -1) {
+          rowIndexes.add(rowIndex);
+        }
+      }
+      view.highlightRows(rowIndexes);
+    }
+    else{
+      JOptionPane.showMessageDialog(view, "No filter applied");
+      view.toFront();}
+
+  }
+}
---
+++ test/TestExample.java
@@ -0,0 +1,116 @@
+// package test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.List;
+import java.text.ParseException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controller.ExpenseTrackerController;
+import model.ExpenseTrackerModel;
+import model.Transaction;
+import view.ExpenseTrackerView;
+
+
+public class TestExample {
+  
+  private ExpenseTrackerModel model;
+  private ExpenseTrackerView view;
+  private ExpenseTrackerController controller;
+
+  @Before
+  public void setup() {
+    model = new ExpenseTrackerModel();
+    view = new ExpenseTrackerView();
+    controller = new ExpenseTrackerController(model, view);
+  }
+
+    public double getTotalCost() {
+        double totalCost = 0.0;
+        List<Transaction> allTransactions = model.getTransactions(); // Using the model's getTransactions method
+        for (Transaction transaction : allTransactions) {
+            totalCost += transaction.getAmount();
+        }
+        return totalCost;
+    }
+
+
+    public void checkTransaction(double amount, String category, Transaction transaction) {
+	assertEquals(amount, transaction.getAmount(), 0.01);
+        assertEquals(category, transaction.getCategory());
+        String transactionDateString = transaction.getTimestamp();
+        Date transactionDate = null;
+        try {
+            transactionDate = Transaction.dateFormatter.parse(transactionDateString);
+        }
+        catch (ParseException pe) {
+            pe.printStackTrace();
+            transactionDate = null;
+        }
+        Date nowDate = new Date();
+        assertNotNull(transactionDate);
+        assertNotNull(nowDate);
+        // They may differ by 60 ms
+        assertTrue(nowDate.getTime() - transactionDate.getTime() < 60000);
+    }
+
+
+    @Test
+    public void testAddTransaction() {
+        // Pre-condition: List of transactions is empty
+        assertEquals(0, model.getTransactions().size());
+    
+        // Perform the action: Add a transaction
+	double amount = 50.0;
+	String category = "food";
+        assertTrue(controller.addTransaction(amount, category));
+    
+        // Post-condition: List of transactions contains only
+	//                 the added transaction	
+        assertEquals(1, model.getTransactions().size());
+    
+        // Check the contents of the list
+	Transaction firstTransaction = model.getTransactions().get(0);
+	checkTransaction(amount, category, firstTransaction);
+	
+	// Check the total amount
+        assertEquals(amount, getTotalCost(), 0.01);
+    }
+
+
+    @Test
+    public void testRemoveTransaction() {
+        // Pre-condition: List of transactions is empty
+        assertEquals(0, model.getTransactions().size());
+    
+        // Perform the action: Add and remove a transaction
+	double amount = 50.0;
+	String category = "food";
+        Transaction addedTransaction = new Transaction(amount, category);
+        model.addTransaction(addedTransaction);
+    
+        // Pre-condition: List of transactions contains only
+	//                the added transaction
+        assertEquals(1, model.getTransactions().size());
+	Transaction firstTransaction = model.getTransactions().get(0);
+	checkTransaction(amount, category, firstTransaction);
+
+	assertEquals(amount, getTotalCost(), 0.01);
+	
+	// Perform the action: Remove the transaction
+        model.removeTransaction(addedTransaction);
+    
+        // Post-condition: List of transactions is empty
+        List<Transaction> transactions = model.getTransactions();
+        assertEquals(0, transactions.size());
+    
+        // Check the total cost after removing the transaction
+        double totalCost = getTotalCost();
+        assertEquals(0.00, totalCost, 0.01);
+    }
+    
+}
